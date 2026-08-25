6fac993972a45803b839aba3373deec28051aae1
emonsingha51-dev <emonsingha51@gmail.com>
2026-08-25 10:43:44 +0600

    Add files via upload

---
+++ threedigits.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main (){
+    int a ,b,c;
+    printf("enter");
+    scanf("%d %d %d",&a,&b,&c);
+    if (a>b){
+          if (a>c){
+            printf("%d",&a);
+
+          }
+          else {
+               if(b>c){
+            printf("%d",b);
+          }
+          }
+        } 
+          else {
+        
+            printf("%d",c);
+          }
+         return 0;
+}
