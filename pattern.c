c47f569839259a79c582b32a3cf43e5adc9e3e34
emonsingha51-dev <emonsingha51@gmail.com>
2026-08-25 10:52:42 +0600

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter your two numbers");
+    scanf("%d ", &n);
+    for (int j = 1; j <= n; j++)
+    {
+
+        for (int i = 1; i <= ; i++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
