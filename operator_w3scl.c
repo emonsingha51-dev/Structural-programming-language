b5ff07eac5899a642fd9ef197121421f24233ac2
emonsingha51-dev <emonsingha51@gmail.com>
2026-09-03 01:34:08 +0600

    Calculate the Total Cost of an Item

---
+++ operator_w3scl.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int itemPrice = 500;
+    int shippingCost = 100;
+
+    int sum;
+
+    sum = itemPrice + shippingCost;
+
+    printf("Total cost = %d", sum);
+
+    return 0;
+}
