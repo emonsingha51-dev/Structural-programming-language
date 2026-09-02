54c56f6fdd7dfa54e64c090a567f086e851b7f7d
emonsingha51-dev <emonsingha51@gmail.com>
2026-09-02 09:31:49 +0600

    create a student report card

---
+++ datatype_w3scl.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main () {
+    int studentID =5;
+    float score = 10.5;
+    char grade = 'A';
+    printf("studentid = %d, score = %f, grade =%c",studentID,score,grade);
+    return 0;
+}
