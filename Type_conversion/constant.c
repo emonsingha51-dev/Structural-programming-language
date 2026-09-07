07460dc9629a977244df02601284ae5bec4777c7
/emonsingha51-dev <emonsingha51@gmail.com>
2026-09-08 02:35:46 +0600

    Update C program

---
+++ Type_conversion/constant.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  const int YEAR = 60;
+  const int MONTHS= 12;
+
+  printf("%d\n", YEAR);
+  printf("%d\n", MONTHS);
+  return 0;
+}
--- nextline.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main () {
-    printf("Hello world! \n");
-    printf("I am learning C");
-    return 0;
-}
