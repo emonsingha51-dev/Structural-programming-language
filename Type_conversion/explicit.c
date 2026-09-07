a8b44cd50bb7e1076b940543676853ca8fee7922
/emonsingha51-dev <emonsingha51@gmail.com>
2026-09-07 23:58:46 +0600

    Solve type conversion problem

---
+++ Type_conversion/explicit.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main () {
+    int x= 10;
+    int y= 3;
+    float z= (float)x/y;
+    printf("Value of z is : %f", z);
+    return 0;
+}
---
+++ Type_conversion/implicit1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+    int num =14.66;
+    printf("Value of num: %d", num);
+    return 0;
+}
