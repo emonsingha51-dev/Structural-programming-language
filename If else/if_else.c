70000e659055de1b7253007d857be69d4b013e14
/emonsingha51-dev <emonsingha51@gmail.com>
2026-09-08 14:22:58 +0600

    Solving if else

---
+++ If else/if_else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+
+    int x = 7;
+    int y = 5;
+
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("x is less than y\n");
+    }
+    return 0;
+}
---
+++ If else/shorthand_if.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main (){
+
+    int age = 20;
+    (age >=18)?printf("You are eligible to vote\n"):printf("You are not eligible to vote\n");
+    return 0;
+}
